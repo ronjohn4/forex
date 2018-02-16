09271e22ed513e9696669382561d7137101b4319
Ron Johnson <ronjohn4@gmail.com>
2018-02-16 06:24:27 -0700

    Breaking point

--- datasources/datautil.py
+++ datasources/datautil.py
@@ -52,15 +52,85 @@ class forexDB():
                   quotecurrency=quotecurrency,
                   value=value)
 
+
     def return_training_data(self):
+        import pandas as pd
+        # df = pandas.DataFrame(data, columns=['Fruit', 'Shop', 'Price'])
+        # df.pivot(index='Fruit', columns='Shop', values='Price')
+
+        df = pd.read_sql_query("SELECT collectiondate, quotecurrency, value FROM Pairs WHERE quotecurrency in ('EUR', 'AOA', 'AED', 'JPY', 'ZAR') ORDER by collectiondate;",
+                               self.__conn)
+
+        # df = pd.read_sql_query("SELECT collectiondate, quotecurrency, value FROM Pairs ORDER by collectiondate;",
+        #                        self.__conn)
+
         data = []
         target = []
+
+        data2 = []
+        target2 = []
+
         cursor = self.__conn.execute(
-            "select value from Pairs where quotecurrency='{0}' order by collectiondate;".format('EUR'))
+            "SELECT collectiondate, quotecurrency, value FROM Pairs WHERE quotecurrency in ('EUR', 'AOA', 'AED', 'JPY', 'ZAR') ORDER by collectiondate;")
         rows = cursor.fetchall()
         for row in rows:
             print(row)
-            data.append(row[0])
-        print('data:', data)
-        print('target:', target)
-        return(data, target)
+            data.append([row[0], row[1], row[2]])
+
+        dp = df.pivot(index='collectiondate', columns='quotecurrency', values='value')
+        # print('df')
+        # print(df)
+
+        # writer = pd.ExcelWriter('pandas_simple.xlsx', engine='xlsxwriter')
+        # dp.to_excel(writer, "sheetx")
+        # writer.save()
+
+        # print('dp')
+        # print(dp)
+        #
+        # print('dp-head')
+        # print(dp.head())
+        #
+        # print('index')
+        # print(dp.index)
+        #
+        # print('values')
+        # print(dp.values)
+
+        print('dp[2:7]')
+        print(dp[2:7])
+
+        p1 = dp[6:7]['AOA'].values
+        print('dp[6:7]', p1)
+
+        p2 = dp[7:8]['AOA'].values
+        print('dp[7:8]', p2)
+
+        print('diff:', p2-p1)
+
+        data2.append(dp[2:7])
+        target2.append(p2 > p1)
+
+        data3 = []
+        target3 = []
+        for i in range(5):
+            data3.append(dp[i:i+1].values)
+
+
+        print('data3')
+        print(data3)
+
+        print('data:', data2)
+        print('target:', target2)
+
+        # dataset_array = data2.values
+
+
+        # print('data:',type(data))
+        # print('data2:',type(data2))
+        # print('target:',type(target))
+        # print('target2:',type(target2))
+
+
+
+        return(data3, target2)
